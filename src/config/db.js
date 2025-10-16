14863af9ce3866779fe18c24139d0bb291e897d5
Isuho Friday <isuho5050@gmail.com>
2025-10-16 10:37:25 +0100

    Cleaned setup, ready for Railway deployment

--- src/config/db.js
+++ src/config/db.js
@@ -2,16 +2,13 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDB = () => {
-   return mongoose
-    .connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+  return mongoose
+    .connect(process.env.MONGODB_URI, {})
     .then((result) => {
       console.log("Connected to DB");
     })
     .catch((err) => {
-      console.log(err);
+      console.error("DB connection error:", err);
       process.exit(1);
     });
 };
